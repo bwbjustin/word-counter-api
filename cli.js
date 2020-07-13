50019a64e085208701f35538140828b58efa7b2f
Justin Pridgen <52604018+hiimjustin000@users.noreply.github.com>
2020-07-13 07:43:00 -0400

    Grammatically correct

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const counter = require("./counter")(process.argv.slice(2).join(" "));
 
-console.log(`${counter.words} words, ${counter.characters} characters`);
+console.log(`${counter.words} ${counter.words != 1 ? "words" : "word"}, ${counter.characters} characters`);
