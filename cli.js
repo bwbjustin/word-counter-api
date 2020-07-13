221d7adacc2dc08ca9f1d19411c7b30b7de4a464
hiimjustin000 <justincpridgen672@gmail.com>
2020-07-13 07:21:51 -0400

    Why am I so dumb

--- cli.js
+++ cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-const counter = require("./counter")(process.argv.slice(2));
+const counter = require("./counter")(process.argv.slice(2).join(" "));
 
 console.log(`${counter.words} words, ${counter.characters} characters`);
